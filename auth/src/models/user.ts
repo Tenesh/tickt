117bd9ab999333e48942db3f3f17783cc66eac94
Tenesh <teneshdev@gmail.com>
2022-05-31 23:37:22 +0800

    Add mongoose model for auth and define user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+// Interface for new user creation.
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// Interface for User Model
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface for User Document
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+
+export {User};
