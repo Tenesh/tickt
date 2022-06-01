f10ca6b76ee7b405cea73b5da94220af7252c87d
Tenesh <teneshdev@gmail.com>
2022-06-02 00:04:27 +0800

    Add new ticket route and test case

---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response, NextFunction} from 'express';
+import {body} from 'express-validator';
+import {requireAuth, validateRequest} from '@ticketeer/common';
+
+
+const router = express.Router();
+
+router.post('/api/tickets', requireAuth,
+    [
+        body('title')
+            .not()
+            .isEmpty()
+            .withMessage('Title is required'),
+        body('price')
+            .isFloat({gt: 0})
+            .withMessage('Price must be greater than 0')
+    ], validateRequest,
+    (req: Request, res: Response, next: NextFunction) => {
+        res.sendStatus(200);
+    });
+
+export {router as createTicketRouter}
