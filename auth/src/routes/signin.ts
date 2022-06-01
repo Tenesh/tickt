5dfecbc12ab1cd3ef35bae1f6c87c8bc0bb45cdb
Tenesh <teneshdev@gmail.com>
2022-06-01 22:27:35 +0800

    Add common package repo.

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,12 +1,10 @@
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
+import {BadRequestError, validateRequest} from '@ticketeer/common';
 
 import {User} from '../models/user';
-import {BadRequestError} from '../errors/bad-request-error';
-import {validateRequest} from "../middlewares/validate-request";
 import {Password} from '../services/password';
-
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,7 @@
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
-
-
-import {BadRequestError} from '../errors/bad-request-error';
-import {validateRequest} from '../middlewares/validate-request';
+import {BadRequestError, validateRequest} from '@ticketeer/common';
 
 import {User} from '../models/user';
 
