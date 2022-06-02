205b8b0809ae67fe43ed2a5f5430aa4a579a7e4e
Tenesh <teneshdev@gmail.com>
2022-06-02 22:27:34 +0800

    Add orders routes setup

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,10 @@ import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
 import {errorHandler, NotFoundError, currentUser} from '@ticketeer/common';
 
+import {createOrderRouter} from './routes/new';
+import {showOrderRouter} from './routes/show';
+import {indexOrderRouter} from './routes';
+import {deleteOrderRouter} from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +19,11 @@ app.use(
     })
 );
 
+app.use(currentUser);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
